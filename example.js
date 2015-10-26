0f8108dddf4e55c53f53f4aec4ebfe19de0bb706
Christian Vadalà <chr.vadala@gmail.com>
2015-10-26 11:47:50 +0100

    add rest layer

--- example.js
+++ example.js
@@ -1,9 +1,57 @@
-var koa = require('koa');
-var app = koa();
+"use strict";
 
+var koa = require('koa');
+var Forester = require('./lib/forester.js');
+
+
+var app = koa();
+var forester = new Forester();
 app.experimental = true;
 
 
+
+
+//example config
+var usersCollection = {
+    "name":"users"
+};
+
+var articlesCollection = {
+    "name":"articles"
+};
+
+
+var dataSource = {
+    "name":"dbrome",
+    "adapter": "mongo",
+    "options": {
+        "connectionUri": "mongodb://forester_db_1:27017"
+    }
+};
+
+var mappings = [
+    {
+        "collection": "users",
+        "datasource": "dbrome"
+    },
+    {
+        "collection": "articles",
+        "datasource": "dbrome"
+    }
+];
+//end example config
+
+
+//example registration
+forester.registerCollection(usersCollection);
+forester.registerCollection(articlesCollection);
+forester.registerDataSource(dataSource);
+forester.registerMapping(mappings[0]);
+forester.registerMapping(mappings[1]);
+//end registration
+
+
+forester.boot(app);
 
 
 app.listen(3000);
