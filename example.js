31011e0a5e061640c601438bc8c3ce6ed838a3b4
enricomarino <enrico.marino@email.com>
2015-11-03 18:17:07 +0100

    add explorer parts

--- example.js
+++ example.js
@@ -3,13 +3,11 @@
 var Forester = require('./lib/forester.js');
 var app = new Forester();
 
-
 app.registerCollection(require('./test/fixture/join-articles-users.json'));
 app.registerCollection(require('./test/fixture/users.json'));
 app.registerCollection(require('./test/fixture/articles.json'));
 app.registerDataSource(require('./test/fixture/db1.json'));
 app.registerMappings(require('./test/fixture/mappings.json'));
-
 
 app.boot();
 
