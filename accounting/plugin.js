2ba10e86f0bab946bf58e6aa3ad32e56eadb320c
Christian Vadalà <chr.vadala@gmail.com>
2015-11-06 11:12:46 +0100

    plugin code clean

--- accounting/plugin.js
+++ accounting/plugin.js
@@ -1,6 +1,6 @@
 "use strict";
 
-export function plugin(forester){
+module.exports = function (forester) {
 
   forester.registerCollection(require(__dirname + "/_users.json"));
   forester.registerCollection(require(__dirname + "/_sessions.json"));
