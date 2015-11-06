a39a6fd778d153914a78199d6d724ff804928509
Christian Vadalà <chr.vadala@gmail.com>
2015-11-06 12:23:50 +0100

    add category collection

--- example.js
+++ example.js
@@ -5,8 +5,10 @@ var app = new Forester();
 
 app.registerCollection(require('./test/fixture/join-articles-users.json'));
 app.registerCollection(require('./test/fixture/users.json'));
+app.registerCollection(require('./test/fixture/category.json'));
 app.registerCollection(require('./test/fixture/articles.json'));
 app.registerCollection(require('./test/fixture/comments.json'));
+
 app.registerDataSource(require('./test/fixture/db1.json'));
 app.registerMappings(require('./test/fixture/mappings.json'));
 
