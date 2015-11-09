6a22f4aeb37c7f671a6af1b14b2d18699c25b6aa
Christian Vadalà <chr.vadala@gmail.com>
2015-11-09 12:40:48 +0100

    implement login structure

--- accounting/plugin.js
+++ accounting/plugin.js
@@ -1,9 +1,38 @@
 "use strict";
 
+import wrap from '../lib/koa-ctx';
+
 module.exports = function (forester) {
 
   forester.registerCollection(require(__dirname + "/_users.json"));
   forester.registerCollection(require(__dirname + "/_sessions.json"));
 
+  forester.registerEndpoint(
+    {
+      collectionName: "_users",
+      method: "post",
+      route: "/login/",
+      middlewares: [wrap(loginMiddleware({
+        usersCollection: forester.collections._users,
+        sessionsCollection: forester.collections._sessions
+      }))],
+      description: "login and create a session token"
+    }
+  );
+
 };
 
+
+export function loginMiddleware({usersCollection, sessionsCollection}) {
+  return async function ({request, response, params}, next) {
+
+    var data = await request.json();
+    response.body = response.body || {};
+
+
+    response.body.data = {token: 2222};
+    response.body.done = true;
+
+    await next;
+  }
+}
