fa40ebbd226b4058d4f2bdda3ee10e09f2894816
Christian Vadalà <chr.vadala@gmail.com>
2015-12-03 18:35:02 +0100

    move example in specific directory

---
+++ example/example.js
@@ -0,0 +1,28 @@
+"use strict";
+
+var Forester = require('./../lib/forester.js');
+var app = new Forester();
+
+app.registerConfig(require('./json/config.json'));
+app.registerCollection(require('./json/join-articles-authors.json'));
+app.registerCollection(require('./json/collections/authors.json'));
+app.registerCollection(require('./json/collections/categories.json'));
+app.registerCollection(require('./json/collections/articles.json'));
+app.registerCollection(require('./json/collections/comments.json'));
+app.registerCollection(require('./json/collections/profiles.json'));
+
+app.registerDataSource(require('./json/db1.json'));
+app.registerMappings(require('./json/mappings.json'));
+
+
+app.boot()
+  .then(() => {
+  app.listen({port: 3000});
+  })
+  .catch((e) => {
+    console.log(e.stack)
+});
+
+
+
+
